1cd8e83cf871d0f363738b080b61aa8dd48902af
zhoufei <zhoufei@meishubao.com>
2020-02-18 19:44:35 +0800

    init

---
+++ src/components/DataTable.vue
@@ -0,0 +1,50 @@
+<template>
+  <datatable :columns="columns" :data="rows" />
+</template>
+
+<script>
+import Vue from "vue";
+import { VuejsDatatableFactory } from "vuejs-datatable";
+
+Vue.use(VuejsDatatableFactory);
+
+export default {
+  name: "HelloWorld",
+  data: {
+    columns: [
+      { label: "id", field: "id" },
+      {
+        label: "Username",
+        field: "user.username",
+        headerClass: "class-in-header second-class"
+      },
+      { label: "First Name", field: "user.firstName" },
+      { label: "Last Name", field: "user.lastName" },
+      { label: "Email", field: "user.email" },
+      {
+        label: "Address",
+        representedAs: ({ address, city, state }) =>
+          `${address}<br />${city}, ${state}`,
+        interpolate: true
+      }
+    ],
+    rows: [
+      {
+        id: 1,
+        user: {
+          username: "dprice0",
+          firstName: "Daniel",
+          lastName: "Price",
+          email: "dprice0@blogs.com"
+        },
+        address: "3 Toban Park",
+        city: "Pocatello",
+        state: "Idaho"
+      }
+    ]
+  }
+};
+</script>
+
+<style scoped>
+</style>
