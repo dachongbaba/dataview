abc435b6a65d50343859ffc142e77552fbe467e1
zhoufei <zhoufei@meishubao.com>
2020-02-19 11:47:02 +0800

    init

--- src/views/Tables.vue
+++ src/views/Tables.vue
@@ -1,5 +1,7 @@
 <template>
-  <datatable :columns="columns" :data="rows" />
+  <div class="container-full">
+    <datatable class="container table" :columns="columns" :data="rows" />
+  </div>
 </template>
 
 <script>
@@ -10,38 +12,40 @@ Vue.use(VuejsDatatableFactory);
 
 export default {
   name: "Tables",
-  data: {
-    columns: [
-      { label: "id", field: "id" },
-      {
-        label: "Username",
-        field: "user.username",
-        headerClass: "class-in-header second-class"
-      },
-      { label: "First Name", field: "user.firstName" },
-      { label: "Last Name", field: "user.lastName" },
-      { label: "Email", field: "user.email" },
-      {
-        label: "Address",
-        representedAs: ({ address, city, state }) =>
-          `${address}<br />${city}, ${state}`,
-        interpolate: true
-      }
-    ],
-    rows: [
-      {
-        id: 1,
-        user: {
-          username: "dprice0",
-          firstName: "Daniel",
-          lastName: "Price",
-          email: "dprice0@blogs.com"
+  data: function () {
+    return {
+      columns: [
+        { label: "id", field: "id" },
+        {
+          label: "Username",
+          field: "user.username",
+          headerClass: "class-in-header second-class"
         },
-        address: "3 Toban Park",
-        city: "Pocatello",
-        state: "Idaho"
-      }
-    ]
+        { label: "First Name", field: "user.firstName" },
+        { label: "Last Name", field: "user.lastName" },
+        { label: "Email", field: "user.email" },
+        {
+          label: "Address",
+          representedAs: ({ address, city, state }) =>
+            `${address}<br />${city}, ${state}`,
+          interpolate: true
+        }
+      ],
+      rows: [
+        {
+          id: 1,
+          user: {
+            username: "dprice0",
+            firstName: "Daniel",
+            lastName: "Price",
+            email: "dprice0@blogs.com"
+          },
+          address: "3 Toban Park",
+          city: "Pocatello",
+          state: "Idaho"
+        }
+      ]
+    }
   }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
+const webpack = require('webpack');
+
 module.exports = {
-  runtimeCompiler: true
+  runtimeCompiler: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default'],
+      }),
+    ],
+  },
 }
