76bc7596ae805cd6b2221ec2dd684af91b844b28
zhoufei <zhoufei@meishubao.com>
2020-02-23 16:42:30 +0800

    ghpages

--- ghpages.js
+++ ghpages.js
@@ -1,6 +1,6 @@
 const ghpages = require('gh-pages');
 
-const repo = process.env.REPO_URL || 'https://newsgitlab.meishubao.com/msb-ai/dataview.git';
+const repo = process.env.REPO_URL || 'https://newsgitlab.meishubao.com/msb-ai/dv.git';
 const src = 'dist';
 const dest = 'public';
 console.info('publish:', repo, src, dest);
