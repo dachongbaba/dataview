23dbd9ed624c3d1b7895c528a17289ec2af546de
zhoufei <zhoufei@meishubao.com>
2020-02-23 15:56:19 +0800

    tables

--- src/config.js
+++ src/config.js
@@ -24,13 +24,12 @@ const defaultFetchs = {
   },
   dataPath: 'payload.content',
   pagePath: 'payload.totalElements',
+  errorPath: '',
 };
 const defaultColumns = [
-  {title: 'id', field: 'id', label: 'id lable', sortable: true, visible: 'true'}, 
-  {title: 'title', field: 'title', label: 'title lable', sortable: true, visible: 'true'}, 
-  {title: 'desc', field: 'desc', label: 'desc lable', sortable: true, visible: 'true'}, 
-  {title: 'ctime', field: 'ctime', callback: 'formatDate|YYYY-MM-DD HH:mm:ss', sort: true}, 
-  {title: 'utime', field: 'utime', callback: 'formatDate|YYYY-MM-DD HH:mm:ss', sort: true},
+  {title: 'Id', data: 'id', sortable: true}, 
+  {title: 'Ctime', data: 'ctime', render: 'formatDate|YYYY-MM-DD HH:mm:ss'}, 
+  {title: 'Utime', data: 'utime', render: 'formatDate|YYYY-MM-DD HH:mm:ss'},
 ];
 const defaultOptions = {};
 
--- src/views/Tables.vue
+++ src/views/Tables.vue
@@ -1,18 +1,6 @@
 <template>
   <div>
-    <code>query: {{ query }}</code>
-    <table ref="table" class="table">
-      <thead>
-          <tr>
-              <th v-for="item in columns" :key="item.field">{{ item.title }}</th>
-          </tr>
-      </thead>
-      <tbody>
-           <tr v-for="row in datas" :key="row.id">
-             <td v-for="col in columns" :key="col.field">{{ row[col.field] }}</td>
-          </tr>
-      </tbody>
-    </table>
+    <table ref="table" class="display" style="width:100%"></table>
   </div>
 </template>
 
@@ -20,8 +8,24 @@
 import config from '../config';
 import _ from 'loadsh';
 import axios from 'axios';
-import 'datatables.net-dt/js/dataTables.dataTables'
+import moment from 'moment';
+import 'datatables.net/js/jquery.dataTables'
 import 'datatables.net-dt/css/jquery.dataTables.css'
+
+/*
+import 'datatables.net-plugins/dataRender/datetime'
+import 'datatables.net-plugins/dataRender/ellipsis'
+import 'datatables.net-plugins/dataRender/intl'
+import 'datatables.net-plugins/dataRender/percentageBars'
+
+import 'datatables.net-plugins/type-detection/currency'
+import 'datatables.net-plugins/type-detection/date-uk'
+import 'datatables.net-plugins/type-detection/file-size'
+import 'datatables.net-plugins/type-detection/formatted-num'
+import 'datatables.net-plugins/type-detection/ip-address'
+import 'datatables.net-plugins/type-detection/num-html'
+import 'datatables.net-plugins/type-detection/numeric-comma'
+*/
 
 export default {
   name: "Tables",
@@ -31,27 +35,17 @@ export default {
     '_options',
     '_querys',
   ],
-
   data: function () {
     return {
+      dt: null,
       fetchs: {},
       columns: [],
       options: {},
       datas: [],
       total: 0,
-      query: {},
     }
   },
 
-  created() { 
-    this.fetchs = JSON.parse(this.$props._fetchs || JSON.stringify(config.defaultDatas.fetchs));
-    this.columns = JSON.parse(this.$props._columns || JSON.stringify(config.defaultDatas.columns));
-    this.options = JSON.parse(this.$props._options || JSON.stringify(config.defaultDatas.options));
-    this.reload();
-  },
-  mounted() {
-    this.jq(this.$refs.table).DataTable({'serverSide': true, data: this.datas});
-  },
   watch: {
     query: {
       handler (query) {
@@ -60,29 +54,93 @@ export default {
       deep: true
     }
   },
+
+  created() { 
+    this.fetchs = JSON.parse(this.$props._fetchs || JSON.stringify(config.defaultDatas.fetchs));
+    this.columns = JSON.parse(this.$props._columns || JSON.stringify(config.defaultDatas.columns));
+    this.options = JSON.parse(this.$props._options || JSON.stringify(config.defaultDatas.options));
+  },
+  mounted() {
+    var that = this;
+    var defaultOptions = {
+      ajaxDataProp: 'data',
+      serverSide: true,
+      ajax: this.reload,
+      search: {
+        search: this.fetchs.params.query
+      }
+    };
+    var options = _.merge({}, defaultOptions, that.options, {
+      columns: _.map(this.columns, (column) => {
+        return this.columnRender.call(that, column);
+      })
+      // columns: this.columns
+    })
+    that.dt = that.jq(that.$refs.table).DataTable(options);
+  },
+  beforeDestroy() {
+    this.dataTable.destroy(true);
+  },
+  
   computed: {
-    reload() {
-      return _.debounce(this.fetchData, 500);
-    },
     jq() {
       return this.jquery || window.jQuery
     },
+    reload() {
+      return _.debounce(this.fetchData, 500);
+    }
   },
 
   methods: {
-    fetchData() {
+    fetchData(data, callback) {
       var vm = this;
       var fetchs = this.fetchs;
-      //fetchs.params.page = query.offset / query.limit;
-      //fetchs.params.sort = `${query.sort},${query.order}`;
+      if (!this.options.searching) {
+        fetchs.params.query = data.search.value;
+      }
+      if (!this.options.paging) {
+        fetchs.params.page = data.start / data.length + 1;
+        fetchs.params.size = data.length || 20;
+      }
+      if (!this.options.ordering) {
+        var index = data.order[0].column;
+        var sort = data.columns[index].data;
+        var order = data.order[0].dir
+      }
+      fetchs.params.sort = `${sort},${order}`;
       axios(fetchs).then(function (response) {
         vm.datas = _.get(response.data, fetchs.dataPath, []);
         vm.total = _.get(response.data, fetchs.pagePath, 0) * 1;
-        console.log('end');
+        vm.error = _.get(response.data, fetchs.errorPath, {});
       }).catch(function (error) {
         console.error(fetch, error);
       }).finally(function () {
-      }); 
+        // vm.dt.clear().rows.add(vm.datas).draw(false);
+        callback({
+          recordsTotal: vm.total,
+          recordsFiltered: vm.total,
+          data: vm.datas,
+          error: vm.error
+        });
+      });
+    },
+    columnRender(column) {
+      if (column.render) {
+        var that = this;
+        var renders = column.render.split('|');
+        var render = that[renders[0]];
+        column.render = function(data) {
+          renders[0] = data;
+          return render.apply(that, renders);
+        }
+      }
+      return column; 
+    },
+    formatDate(value, fmt = 'YYYY-MM-DD') {
+      if (!value) return '';
+      var date = new Date(value/1);
+      var text = moment(date).format(fmt);
+      return text;
     }
   },
 };
