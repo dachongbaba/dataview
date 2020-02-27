0587f5ebe9e4f9a283caa938dbbe719966c8ae5b
zhoufei <zhoufei@meishubao.com>
2020-02-27 10:47:11 +0800

    updage package

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,25 @@
 const webpack = require('webpack');
+const NODE_ENV = process.env.NODE_ENV
 var baseUrl = '';
 module.exports = {
   publicPath: baseUrl,
   runtimeCompiler: true,
   configureWebpack: {
+    externals: {
+    },
+    output:
+      NODE_ENV !== 'development'
+        ? {
+          filename: 'js/[name].[hash:10].js',
+          chunkFilename: 'js/[name].[chunkhash:10].js'
+        }
+        : {},
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000
+      }
+    },
     plugins: [
       new webpack.ProvidePlugin({
         $: 'jquery/dist/jquery.min',
@@ -12,7 +28,6 @@ module.exports = {
       }),
     ],
   },
-
   devServer: {
     proxy: 'https://www.xiaoxiongmeishu.com'
   }
