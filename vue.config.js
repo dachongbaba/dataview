d191d0d362ef116a514ba36bceb2f4b928ede826
zhoufei <zhoufei@meishubao.com>
2020-02-27 18:23:21 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,21 @@
 const webpack = require('webpack');
-const NODE_ENV = process.env.NODE_ENV
+const NODE_ENV = process.env.NODE_ENV;
+
 var baseUrl = '';
 module.exports = {
   publicPath: baseUrl,
   runtimeCompiler: true,
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html',
-      title: 'Dashboard Page',
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    }
-  },
   configureWebpack: {
-    output: NODE_ENV == 'production' ? {
+    output: {
       filename: 'js/[name].[hash:10].js',
       chunkFilename: 'js/[name].[chunkhash:10].js'
-    } : {},
-    optimization: NODE_ENV == 'production' ? {
+    },
+    optimization: {
       splitChunks: {
         minSize: 10000,
         maxSize: 250000
       }
-    } : {},
+    },
     plugins: [
       new webpack.ProvidePlugin({
         $: 'jquery/dist/jquery.min',
