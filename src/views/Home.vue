c659b9df563b56c5ad7db7c7f96e5d034877c473
zhoufei <zhoufei@meishubao.com>
2020-02-27 20:39:58 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="jumbotron">
-    <router-link class="m-5" to="/views?_title=Tables&_view=table">Table View</router-link>
-    <router-link class="m-5" to="/views?_title=Tables&_view=card">Card View</router-link>
-    <router-link class="m-5" to="/views?_title=Tables&_view=list">List View</router-link>
+    <ul class="nav">
+      <li class="nav-item">
+        <router-link class="nav-link" to="/views?_title=Tables&_view=table">Table View</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/views?_title=Tables&_view=card">Card View</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/views?_title=Tables&_view=list">List View</router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
